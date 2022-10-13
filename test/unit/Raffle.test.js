e9fc6ebbe3043a013af3f08f32840138653ed272
0xkmg <83229800+0xKMG@users.noreply.github.com>
2022-10-13 13:57:16 +0800

    write basic unit tests for the raffle contract

--- test/unit/Raffle.test.js
+++ test/unit/Raffle.test.js
@@ -96,6 +96,7 @@ const { developmentChains, networkConfig } = require("../../helper-hardhat-confi
           await raffle.enterRaffle({ value: raffleEntranceFee });
           await network.provider.send("evm_increaseTime", [interval.toNumber() + 1]);
           await network.provider.send("evm_mine", []);
+
           const { upkeepNeeded } = await raffle.callStatic.checkUpkeep([]);
           assert.equal(upkeepNeeded, true);
         });
@@ -139,6 +140,62 @@ const { developmentChains, networkConfig } = require("../../helper-hardhat-confi
           await expect(
             vrfCoordinatorV2Mock.fulfillRandomWords(1, raffle.address) // reverts if not fulfilled
           ).to.be.revertedWith("nonexistent request");
+        });
+        // This test simulates users entering the raffle and wraps the entire functionality of the raffle
+        // inside a promise that will resolve if everything is successful.
+        // An event listener for the WinnerPicked is set up
+        // Mocks of chainlink keepers and vrf coordinator are used to kickoff this winnerPicked event
+        // All the assertions are done once the WinnerPicked event is fired
+        it("picks a winner, resets, and sends money", async () => {
+          let additionalEntrances = 3; // to test
+          let startingIndex = 2;
+          for (let i = startingIndex; i < startingIndex + additionalEntrances; i++) {
+            // i = 2; i < 5; i=i+1
+            raffle = raffleContract.connect(accounts[i]); // Returns a new instance of the Raffle contract connected to player
+            await raffle.enterRaffle({ value: raffleEntranceFee });
+          }
+          const startingTimeStamp = await raffle.getLastTimeStamp(); // stores starting timestamp (before we fire our event)
+
+          // This will be more important for our staging tests...
+          await new Promise(async (resolve, reject) => {
+            raffle.once("WinnerPicked", async () => {
+              // event listener for WinnerPicked
+              console.log("WinnerPicked event fired!");
+              // assert throws an error if it fails, so we need to wrap
+              // it in a try/catch so that the promise returns event
+              // if it fails.
+              try {
+                // Now lets get the ending values...
+                const recentWinner = await raffle.getRecentWinner();
+                const raffleState = await raffle.getRaffleState();
+                const winnerBalance = await accounts[2].getBalance();
+                const endingTimeStamp = await raffle.getLastTimeStamp();
+                await expect(raffle.getPlayer(0)).to.be.reverted;
+                // Comparisons to check if our ending values are correct:
+                assert.equal(recentWinner.toString(), accounts[2].address);
+                assert.equal(raffleState, 0);
+                assert.equal(
+                  winnerBalance.toString(),
+                  startingBalance // startingBalance + ( (raffleEntranceFee * additionalEntrances) + raffleEntranceFee )
+                    .add(raffleEntranceFee.mul(additionalEntrances).add(raffleEntranceFee))
+                    .toString()
+                );
+                assert(endingTimeStamp > startingTimeStamp);
+                resolve(); // if try passes, resolves the promise
+              } catch (e) {
+                reject(e); // if try fails, rejects the promise
+              }
+            });
+
+            // kicking off the event by mocking the chainlink keepers and vrf coordinator
+            const tx = await raffle.performUpkeep("0x");
+            const txReceipt = await tx.wait(1);
+            const startingBalance = await accounts[2].getBalance();
+            await vrfCoordinatorV2Mock.fulfillRandomWords(
+              txReceipt.events[1].args.requestId,
+              raffle.address
+            );
+          });
         });
       });
     });
